7f362262ac8898ff9fea3f3ee89e5b124edf874a
Betul <betulkoru06@gmail.com>
2024-10-03 11:58:26 +0300

    feat: login page route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import {Route, Routes} from 'react-router-dom';
 import './App.css';
 import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/loginPage';
 
 function App() {
 
@@ -8,8 +9,8 @@ function App() {
 
     <Routes>
       <Route index element={ <IndexPage />} />
+      <Route path='/login' element={<LoginPage />} />
     </Routes>
-
 
   )
 }
