6f9e8f9677ab65721c413a8fe42a74e481b0d195
Betul <betulkoru06@gmail.com>
2024-10-03 12:05:19 +0300

    feat: seperate header component

--- client/src/pages/indexPage.jsx
+++ client/src/pages/indexPage.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import Header from "../Header";
 
 export default function IndexPage() {
     
@@ -6,48 +6,8 @@ export default function IndexPage() {
 
         <div>
 
-            {/* HEADER SECT */}
-            <header className='p-4 flex justify-between'>
-                <a href="" className="flex items-center gap-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className='w-8 h-8'>
-                        <path d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z" />
-                    </svg>
-                    <span className='font-bold text-xl'>
-                        airbnb
-                    </span>
-                </a>
-
-                <div className='flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300'>
-                    <div>Anywhere</div>
-                    <div className="border-l border-gray-300"></div>
-                    <div>Any week</div>
-                    <div className="border-l border-gray-300"></div>
-                    <div>Add guests</div>
-                    <button className='bg-primary text-white p-1 rounded-full'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                        </svg>
-                    </button>
-                </div>
-
-                <Link to={'/login'} className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4'>
-
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-
-                    <div className='bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 relative top-1">
-                            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
-                        </svg>
-
-                    </div>
-
-                </Link>
-
-            </header>
-            {/* HEADER SECT ENDS*/}
+            <Header />
 
         </div>
     )
-}
+};
