e714ecfb6baed90e76aef5816a84540da7e5d21d
Betul <betulkoru06@gmail.com>
2024-10-03 11:54:18 +0300

    fix: render issue fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import {Route, Routes} from 'react-router-dom';
 import './App.css';
-import indexPage from './pages/indexPage';
+import IndexPage from './pages/indexPage';
 
 function App() {
 
   return (
 
     <Routes>
-      <Route index element={ <indexPage />} />
+      <Route index element={ <IndexPage />} />
     </Routes>
 
 
