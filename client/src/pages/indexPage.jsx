d73b9836e5136fb1d38c6c566c0de7d7d7bd9b6c
Betul <betulkoru06@gmail.com>
2024-10-03 12:00:12 +0300

    feat: login link routing onclick

--- client/src/pages/indexPage.jsx
+++ client/src/pages/indexPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function IndexPage() {
     
     return (
@@ -28,7 +30,7 @@ export default function IndexPage() {
                     </button>
                 </div>
 
-                <div className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4'>
+                <Link to={'/login'} className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4'>
 
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -41,7 +43,7 @@ export default function IndexPage() {
 
                     </div>
 
-                </div>
+                </Link>
 
             </header>
             {/* HEADER SECT ENDS*/}
