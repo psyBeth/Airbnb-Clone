74682b6a469234bbdff2ca90a9bbb7f157e1c3ae
Betul <betulkoru06@gmail.com>
2024-10-04 22:29:03 +0300

    feat: register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/loginPage';
 import Layout from './Layout';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
 
@@ -12,9 +13,10 @@ function App() {
       <Route path='/' element={<Layout />} >
         <Route index element={<IndexPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Route>
     </Routes>
-    
+
   )
 }
 
