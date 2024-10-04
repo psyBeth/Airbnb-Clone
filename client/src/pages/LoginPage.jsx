0acd032c7f870f476fe37c2bf76f7de92c46be4c
Betul <betulkoru06@gmail.com>
2024-10-04 22:25:37 +0300

    feat: register link added 2 loginpage

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
     return(
 
@@ -8,6 +10,10 @@ export default function LoginPage() {
                 <input type="email" placeholder="your@email.com" />
                 <input type="password" placeholder="password" />
                 <button className="primary">Login</button>
+                <div className="text-center py-2 text-gray-500">
+                    Don't have an account yet?
+                    <Link className="underline text-black" to={'/register'}> Register now.</Link>
+                </div>
             </form>
             </div>
         </div>
