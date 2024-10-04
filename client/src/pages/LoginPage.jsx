0cd3ef2da3e4cd80248b8b89e04417d7defba9fb
Betul <betulkoru06@gmail.com>
2024-10-04 22:21:58 +0300

    style: login style + css classes

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,13 +1,15 @@
 export default function LoginPage() {
     return(
 
-        <div className="mt-4 ">
-            <h1 className="text-4xl text-center">Login</h1>
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="-mt-64">
+            <h1 className="text-4xl text-center mb-4">Login</h1>
             <form className="max-w-md mx-auto ">
                 <input type="email" placeholder="your@email.com" />
                 <input type="password" placeholder="password" />
-                <button>Login</button>
+                <button className="primary">Login</button>
             </form>
+            </div>
         </div>
 
     )
