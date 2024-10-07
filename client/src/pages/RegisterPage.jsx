065b0edf34c1e3df30be9699a5f392b8b8bf0459
Betul <betulkoru06@gmail.com>
2024-10-07 21:19:43 +0300

    feat: name input added to register

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,6 +7,7 @@ export default function RegisterPage() {
             <div className="-mt-64">
             <h1 className="text-4xl text-center mb-4">Register</h1>
             <form className="max-w-md mx-auto ">
+                <input type="text" placeholder="John Doe" />
                 <input type="email" placeholder="your@email.com" />
                 <input type="password" placeholder="password" />
                 <button className="primary">Login</button>
