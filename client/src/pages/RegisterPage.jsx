70fcc9cc678973f951309feec713062769f4cf85
Betul <betulkoru06@gmail.com>
2024-10-16 20:06:04 +0300

    feat: register form onSubmit

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
 
 export default function RegisterPage() {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    function registerUser(ev) {
+        ev.preventDefault();
+        axios.get('http://localhost:4000/test')
+    }
 
     return (
 
         <div className="mt-4 grow flex items-center justify-around">
             <div className="-mt-64">
                 <h1 className="text-4xl text-center mb-4">Register</h1>
-                <form className="max-w-md mx-auto ">
+                <form className="max-w-md mx-auto" onSubmit={registerUser} >
                     <input type="text" z
                         placeholder="John Doe"
                         value={name}
