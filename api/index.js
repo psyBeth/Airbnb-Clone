3ade64228832d8807b5e24f9a895f049014eed86
Betul <betulkoru06@gmail.com>
2024-10-21 17:53:22 +0300

    feat:c ors usage in api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173',
+}))
 
 app.get('/test', (req, res) => {
     res.json('test ok');
