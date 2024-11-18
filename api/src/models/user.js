0f8721d27c27173320bfb595513fedc68ce7f0db
Betul <betulkoru06@gmail.com>
2024-11-18 11:32:49 +0300

    feat: api user model

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -5,27 +5,43 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
 
     username: {
-
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
     },
 
     firstName: {
-
+        type: String,
+        trim: true,
+        required: true,
     },
 
     lastName: {
-
+        type: String,
+        trim: true,
+        required: true,
     },
 
     email: {
-
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true,
+        // validate: ... (validation - pre(save))
     },
 
     password: {
-
+        type: String,
+        trim: true,
+        required: true
     },
 
     isActive: {
-
+        type: Boolean,
+        default: true
     },
 
 }, {
