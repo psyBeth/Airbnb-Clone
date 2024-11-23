4cde5cdf99448d874d088836c94d01ff1d478d81
Betul <betulkoru06@gmail.com>
2024-11-23 19:38:13 +0300

    feat: user listing (not complete)

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -5,6 +5,14 @@ const User = require('../models/user');
 module.exports = {
     list: async (req, res) => {
 
+        const data = await res.getModelList(User, [])   //!!! listing  and pagination file
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(User, []), //!!
+            data
+        });
+
     },
 
     create: async (req, res) => {
