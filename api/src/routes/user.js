c82a24e31b816712c673853860565acf8d7d59fa
Betul <betulkoru06@gmail.com>
2024-11-24 16:04:52 +0300

    feat: user router

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const router = require('express').Router();
+
+//? permissions ?????
+
+const user = require('../controllers/user');
+
+//! URL will be /users
+
+router.route('/')
+    .get(user.list)
+    .post(user.create);
+
+router.route('/:id')
+    .get(user.list)
+    .put(user.update)
+    .patch(user.update)
+    .delete(user.delete);
+
+module.exports = router;
