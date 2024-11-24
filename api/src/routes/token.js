0fc2ded610c0868b59336522767bf36cb3224779
Betul <betulkoru06@gmail.com>
2024-11-24 15:58:41 +0300

    feat: token router

--- api/src/routes/token.js
+++ api/src/routes/token.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const router = require('express').Router();
+
+//? permissions???
+
+const token = require('../controllers/token');
+
+//! URL will be /tokens
+
+router.route('/')
+    .get(token.list)
+    .post(token.create);
+
+router.route('/:id')
+    .get(token.read)
+    .put(token.update)
+    .patch(token.update)
+    .delete(token.delete);
+
+module.exports = router;
