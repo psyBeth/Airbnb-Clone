22673cc4938f97a07b86e9959e12fba35ef6fc4d
Betul <betulkoru06@gmail.com>
2024-11-30 22:15:49 +0300

    feat: logger intro

--- api/src/middlewares/logger.js
+++ api/src/middlewares/logger.js
@@ -2,3 +2,15 @@
 
 // $ yarn add morgan
 // app.use(logger);
+
+//! INTEGRATE
+
+const morgan = require('morgan');
+const fs = require('node:fs');
+
+const now = new Date();
+const today = now.toISOString().split('T')[0];
+
+module.exports = morgan('combined', {
+    stream: fs.createWriteStream(`./logs/${today}.log`, { flags: 'a+' })
+});
