3f11db8f2bce62fad33de48dac9e6e43c1f3e64b
Betul <betulkoru06@gmail.com>
2024-11-30 22:13:15 +0300

    feat: upload intro

--- api/src/middlewares/upload.js
+++ api/src/middlewares/upload.js
@@ -1,3 +1,15 @@
 'use strict'
 
 // app.use(upload.array('fieldName'));
+//! INTEGRATE
+
+const multer = require('multer');
+
+module.exports = multer({
+    storage: multer.diskStorage({
+        destination:'./upload',
+        filename: function(req, file, returnCallback) {
+            returnCallback(null, file.originalname)
+        }
+    })
+});
