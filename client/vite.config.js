6a419e23078b54b1316cf841bd04f37bc4adb84d
Betul <betulkoru06@gmail.com>
2024-12-07 11:25:56 +0300

    fix: proxy config vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    server: {
+        proxy: {
+            '/register': {
+                target: 'http://localhost:4000', // backend server
+                changeOrigin: true,
+                secure: false
+            }
+        }
+    }
+});
