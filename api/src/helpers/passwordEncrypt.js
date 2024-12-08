b3c650ed9a095127241a9a06a7550f4988bb2a19
Betul <betulkoru06@gmail.com>
2024-12-08 14:07:15 +0300

    password encrypt updated

--- api/src/helpers/passwordEncrypt.js
+++ api/src/helpers/passwordEncrypt.js
@@ -2,12 +2,23 @@
 
 // passwordEncrypt(password:string):
 
-const { pbkdf2Sync } = require('node:crypto'),
+const { pbkdf2Sync, randomBytes } = require('node:crypto'),
     keyCode = process.env.SECRET_KEY,
-    loopCount = 1000,
+    loopCount = 100000,
     charCount = 32,
     encType = 'sha512';
 
-module.exports = function (password) {
-    return pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString('hex')
+// module.exports = function (password) {
+//     return pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString('hex')
+// };
+
+module.exports.hashPassword = function (password) {
+    const salt = randomBytes(16).toString('hex');
+    const hash = pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString('hex');
+    return { salt, hash};
+};
+
+module.exports.verifyPassword = function (password, storedHash, storedSalt) {
+    const hash = pbkdf2Sync(password, storedSalt, loopCount, charCount, encType).toString('hex');
+    return hash === storedHash; 
 };
