e2e0909fd350a7d3fff0b9e155aa483be8478927
Betul <betulkoru06@gmail.com>
2024-12-08 20:18:12 +0300

    feat: auth controller intro

---
+++ api/src/controllers/auth.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const User = require('../models/user');
+const Token = require('../models/token');
+const passwordEncrypt = require('../helpers/passwordEncrypt');
+
+module.exports = {
+    
+    login: async (req, res) => {
+
+    },
+
+    refresh: async (req, res) => {
+
+    },
+
+    logout: async (req, res) => {
+        
+    }
+
+};
