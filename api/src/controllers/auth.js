cb9b426abb239512ef05184ce83b41e8390b6a9d
Betul <betulkoru06@gmail.com>
2024-12-08 20:27:32 +0300

    feat: auth ctrl logout

--- api/src/controllers/auth.js
+++ api/src/controllers/auth.js
@@ -15,7 +15,27 @@ module.exports = {
     },
 
     logout: async (req, res) => {
-        
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Token: Logout"
+            #swagger.description = 'Delete token-key.'
+        */       
+       
+        const auth = req.header?.authorization || null;  // Token ...tokenKey... // Bearer ...accessToken...
+        const tokenKey = auth ? auth.split(' ') : null;  // ['Token', '...tokenKey...'] // ['Bearer', '...accessToken...']
+
+        let message = null, resuly = {};
+
+        if(tokenKey && tokenKey[0] == 'Token') {
+            result = await Token.deleteOne({token: tokenKey[1]});
+            message = 'Token deleted, logout OK.'
+        }
+
+        res.send({
+            error: false,
+            message,
+            result
+        });
     }
 
 };
