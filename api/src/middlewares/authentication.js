a4a8696fb68c9d452e0a1d74b29f8a95ea2f493b
Betul <betulkoru06@gmail.com>
2024-12-09 17:25:05 +0300

    feat: JWT added to auth middleware

--- api/src/middlewares/authentication.js
+++ api/src/middlewares/authentication.js
@@ -6,11 +6,20 @@ module.exports = async (req, res, next) => {
     const auth = req.headers?.authorization || null;  // Token ...tokenKey... // Bearer ...accessToken...
     const tokenKey = auth ? auth.split(' ') : null;  // ['Token', '...tokenKey...'] // ['Bearer', '...accessToken...']
 
-    if (tokenKey && tokenKey[0] === 'Token') {
+    if (tokenKey) {
 
-        //! Simple Token
-        const tokenData = await Token.findOne({ token: tokenKey[1] }).populate('userId');
-        req.user = tokenData ? tokenData.userId : undefined;
+        if (tokenKey[0] == 'Token') {
+
+            //! Simple Token
+            const tokenData = await Token.findOne({ token: tokenKey[1] }).populate('userId');
+            req.user = tokenData ? tokenData.userId : undefined;
+
+        } else if (tokenKey[0] == 'Bearer') {
+
+            //! JWT
+            jwt.verify(tokenKey[1], process.env.ACCESS_KEY, (err, userData) => req.user = userData);
+
+        }
 
     };
 
